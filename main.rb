329b8e1d3f08b77ae154960d49ec7d808b64b62b
kazuki <moroku0519@gmail.com>
2014-09-06 10:18:16 +0900

    空いている部室情報取得api追加

--- main.rb
+++ main.rb
@@ -12,8 +12,31 @@ set :environment, :production
 ActiveRecord::Base.configurations = YAML.load_file('database.yml')
 ActiveRecord::Base.establish_connection('development')
 
+configure do
+  # logging is enabled by default in classic style applications,
+  # so `enable :logging` is not needed
+  file = File.new("#{settings.root}/log/#{settings.environment}.log", 'a+')
+  file.sync = true
+  use Rack::CommonLogger, file
+end
+
+
+
+
 class Practices < ActiveRecord::Base
 end
+
+day = Time.now
+  today = day.strftime("%m月%d日")
+
+  week = ["(日)","(月)","(火)","(水)","(木)","(金)","(土)"]
+
+  for i in 0..6 do
+    t_week = week[i] if day.strftime("%w") == i.to_s
+  end 
+
+  t_day = today+t_week.gsub("(","（").gsub(")","）")
+  #半角括弧を全角括弧に変換
 
 get '/Monday.json' do
   content_type :json, :charset => 'utf-8'
@@ -55,4 +78,16 @@ get '/Sunday.json' do
   content_type :json, :charset => 'utf-8'
   practices = Practices.all.where("date like '%（日）%'")
   practices.to_json(:root => false)
+end
+
+get '/available_room.json' do
+  content_type :json, :charset => 'utf-8'
+  practices = Practices.select("date,period,room").where("date = ?",t_day).where("band IS NULL")
+  practices.to_json(:root => false)
+end
+
+get '/available_room.xml' do
+  content_type :xml, :charset => 'utf-8'
+  practices = Practices.select("date,period,room").where("date = ?",t_day).where("band IS NULL")
+  practices.to_xml(:root => false)
 end
